440f3edb817686f5c64823a8dacb5ea601f0d0e6
Pavel Azangue <155538939+raketa25@users.noreply.github.com>
2026-06-10 12:40:52 +0000

    band name project

---
+++ Projects portfolio/bandName.py
@@ -0,0 +1,7 @@
+print("="*50)
+print("WELCOME TO THE BAND NAME GENERATOR!")
+print("="*50)
+city = input("What's the name of the city you grew up in?> ")
+pet = input("What's the name of your pet?> ")
+
+print(f"Your band name could be: {city} {pet}")
