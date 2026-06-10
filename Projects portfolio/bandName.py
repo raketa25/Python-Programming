9c7265a60b3286ca9d3ab4c941c185d3188a63b0
Pavel Azangue <155538939+raketa25@users.noreply.github.com>
2026-06-10 12:43:42 +0000

    band name project modification

--- Projects portfolio/bandName.py
+++ Projects portfolio/bandName.py
@@ -1,3 +1,7 @@
+"""
+This is a simple program that generates a band name based on the user's input. It asks the user for the name of the city they grew up in and the name of their pet, then combines those two pieces of information to create a potential band name. The program uses string concatenation to combine the city and pet names, and it displays the result to the user.
+"""
+
 print("="*50)
 print("WELCOME TO THE BAND NAME GENERATOR!")
 print("="*50)
